8487383c85fa073153d22af94aeb947e0411daae
Nicolas Guerrero <stephenguerrero43@gmail.com>
2022-12-20 13:56:43 -0500

    Only use format feature

--- src/init.rs
+++ src/init.rs
@@ -2,19 +2,12 @@ extern crate ffmpeg_next as ffmpeg;
 
 use std::error::Error;
 
-use ffmpeg::{
-  error::register_all as register_all_errors,
-};
-
 use super::ffi::init_logging;
 
 /// Initialize global ffmpeg settings. This also intializes the
 /// logging capability and redirect it to `tracing`.
 pub fn init() -> Result<(), Box<dyn Error>> {
   ffmpeg::init()?;
-	
-  // Or error messages will be empty.
-  register_all_errors();
 
   // Redirect logging to the Rust `tracing` crate.
   init_logging();
