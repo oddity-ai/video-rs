cdb237cc0ac62f12222bc6883238678ac6f1955f
Gerwin van der Lugt <account+git@gerwin3.com>
2023-04-20 15:33:09 +0200

    added some `inline` annotations in `packet`

--- src/packet.rs
+++ src/packet.rs
@@ -14,33 +14,39 @@ pub struct Packet {
 
 impl Packet {
     /// Get packet PTS (presentation timestamp).
+    #[inline]
     pub fn pts(&self) -> Time {
         Time::new(self.inner.pts(), self.time_base)
     }
 
     /// Get packet DTS (decoder timestamp).
+    #[inline]
     pub fn dts(&self) -> Time {
         Time::new(self.inner.dts(), self.time_base)
     }
 
     /// Get packet duration.
+    #[inline]
     pub fn duration(&self) -> Time {
         Time::new(Some(self.inner.duration()), self.time_base)
     }
 
     /// Set packet PTS (presentation timestamp).
+    #[inline]
     pub fn set_pts(&mut self, timestamp: &Time) {
         self.inner
             .set_pts(timestamp.aligned_with_rational(self.time_base).into_value());
     }
 
     /// Set packet DTS (decoder timestamp).
+    #[inline]
     pub fn set_dts(&mut self, timestamp: &Time) {
         self.inner
             .set_dts(timestamp.aligned_with_rational(self.time_base).into_value());
     }
 
     /// Set duration.
+    #[inline]
     pub fn set_duration(&mut self, timestamp: &Time) {
         if let Some(duration) = timestamp.aligned_with_rational(self.time_base).into_value() {
             self.inner.set_duration(duration);
