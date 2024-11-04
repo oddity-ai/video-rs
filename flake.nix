100fbd258846133517b6237d587df9e95018464d
Gerwin van der Lugt <account+git@gerwin3.com>
2024-11-04 10:49:07 +0100

    updated to ffmpeg 7.1

--- flake.nix
+++ flake.nix
@@ -8,38 +8,46 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-          nativeBuildInputs = with pkgs; [
-            (pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        nativeBuildInputs = with pkgs; [
+          (pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
 
-            # Used by ffmpeg-next-sys to find ffmpeg libraries.
-            pkg-config
-            # Used by ffmpeg-next-sys via bindgen to generate bindings.
-            llvmPackages.libclang
+          # Used by ffmpeg-next-sys to find ffmpeg libraries.
+          pkg-config
+          # Used by ffmpeg-next-sys via bindgen to generate bindings.
+          llvmPackages.libclang
 
-            # Without this bindgen will not be able to find libclang and won't be
-            # able to generate bindings.
-            rustPlatform.bindgenHook
-          ];
-          buildInputs = with pkgs; [
-            ffmpeg
-          ];
-        in
-        with pkgs;
-        {
-          devShells.default = mkShell {
-            inherit buildInputs nativeBuildInputs;
-            # This is needed or rust-analyzer will not work correctly.
-            # Source: https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570
-            RUST_SRC_PATH = "${pkgs.rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; }}/lib/rustlib/src/rust/library";
-          };
-        }
-      );
+          # Without this bindgen will not be able to find libclang and won't be
+          # able to generate bindings.
+          rustPlatform.bindgenHook
+        ];
+        buildInputs = with pkgs; [
+          ffmpeg
+        ];
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          inherit buildInputs nativeBuildInputs;
+          # This is needed or rust-analyzer will not work correctly.
+          # Source: https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570
+          RUST_SRC_PATH = "${
+            pkgs.rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; }
+          }/lib/rustlib/src/rust/library";
+        };
+      }
+    );
 }
